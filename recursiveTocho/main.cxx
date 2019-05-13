9a93ab4889dc62128a729533b42e5e968e8f06ea
Marchusky <45201955+Marchusky@users.noreply.github.com>
2019-05-13 11:35:55 +0200

    recursive homework

--- recursiveTocho/main.cxx
+++ recursiveTocho/main.cxx
@@ -1,6 +1,28 @@
 #include "graphics.h"
 #include <iostream>
 
+
+//                   Drawing Exercises                  //
+
+//void Triangle(const point2d &a,const point2d &b, const point2d &c)
+//{
+//	line(a, b, c);
+//}
+
+struct point2d {
+	float x;
+	float y;
+};
+
+//void slevskyTriangle(a, b, c, level) {
+//	if (level < 6)
+//	{
+//		Triangle(a, b, c);
+//
+//
+//	}
+//}
+
 void concentric_circles(
 	int level,
 	int x,
@@ -11,6 +33,7 @@ void concentric_circles(
 	if (level < 5) {
 		
 		circle(x, y, radius);
+
 		//circle(x - radius/2, y, radius / D);
 		//circle(x + radius/2, y, radius / D);
 		//circle(x, y - radius / 2, radius / D);
@@ -27,6 +50,42 @@ void concentric_circles(
 	}
 }
 
+void line_entreTres(int level,
+	int x1,
+	int y1,
+	int x2,
+	int y2)
+{
+	if (level < 5) {
+		line(x1, y1, x2, y2);
+
+		line_entreTres(level+1, x1, y1 + 10, x2 - (x2 / 3), y2 + 10);
+
+
+	}
+}
+
+//                     Normal Exercises                  //
+
+int numTrues(bool vec[], int sizeVec, unsigned int index)
+{
+
+	if (index < sizeVec) 
+	{
+		int count = numTrues(vec, sizeVec, index + 1);
+		if (vec[index] == true) 
+		{
+			count++;
+		}
+
+		return count;
+		
+	}
+	else{
+		return 0;
+	}
+	
+}
 
 
 void main()
@@ -38,19 +97,19 @@ void main()
 	void rectangle(int left, int top, int right, int bottom);
 	void circle(int x, int y, int radius);
 	*/
+	//                   Drawing Exercises                  //
 
 	initwindow(1000, 1000, "Graphics");
 
 	int level = 0;
-	concentric_circles(level, 500, 500, 500);
+	//concentric_circles(level, 500, 500, 400);
+	//line_entreTres(level, 1, 1, 1000, 1);
 
-	// polla
-	/*circle(200, 600, 100);
-	circle(400, 600, 100);
-	line(200, 500, 200, 100);
-	line(400, 500, 400, 100);
-	line(200, 100, 300, 50);
-	line(400, 100, 300, 50);*/
+
+	//                     Normal Exercises                  //
+	bool vec[] = { true,true,false,true,false,false,false };
+	int count = numTrues(vec, 7, 0);
+	std::cout << "There are " << count << " trues in vec." << std::endl;
 
 
 	system("pause");
